6d506416e503a3e27c9791a28740899945f64653
Maxowino <maxwellowino27@gmail.com>
2024-09-28 11:55:38 +0300

    index change +css

--- pages/signin.php
+++ pages/signin.php
@@ -9,7 +9,7 @@ $Objmenus->main_menu();
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>User Sign-Up and Display</title>
+        <title>Register</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body class="sign">
