20df1542689cd739bcab71c7216dfa4fee4fc25e
Maxowino <maxwellowino27@gmail.com>
2024-10-04 22:50:50 +0300

    signin changes

--- pages/signin.php
+++ pages/signin.php
@@ -8,11 +8,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>User Sign-Up and Display</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+        <style>
+        
+        .form-container {
+            background-color: black; 
+            padding: 30px; 
+            border-radius: 8px; 
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); 
+        }
+    </style>
     </head>
     <body class="sign">
 
-        <div class="container mt-5">
-            <!-- Sign Up Form -->
+        <div class="container mt-5" style="padding: 30px;border: radius 8px;color:black">
+        
             <div class="row">
                 <div class="col-md-6 offset-md-3">
                     <h2 class="text-center">Sign-Up Form</h2>
